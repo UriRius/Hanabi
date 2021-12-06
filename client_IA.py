100681ebb1e6a842bf3becf9a7be0be02598f58c
oriol.rius <oriol.rius@estudiantat.upc.edu>
2021-12-06 12:30:38 +0100

    fixing discard

--- client_IA.py
+++ client_IA.py
@@ -52,8 +52,7 @@ def manageInput():
             try:
                 cardStr = command.split(" ")
                 cardOrder = int(cardStr[1])
-                position = int(cardStr[2])
-                s.send(GameData.ClientPlayerPlayCardRequest(playerName, cardOrder, position).serialize())
+                s.send(GameData.ClientPlayerPlayCardRequest(playerName, cardOrder).serialize())
             except:
                 print("Maybe you wanted to type 'play <num> <pile position>'?")
                 continue
@@ -119,8 +118,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 print(p.toString())
             print("Table cards: ")
             for pos in data.tableCards:
-                print("[ ")
-                for c in pos:
+                print(pos + ": [ ")
+                for c in data.tableCards[pos]:
                     print(c.toString() + " ")
                 print("]")
             print("Discard pile: ")
@@ -150,6 +149,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 print("Your cards with value " + str(data.value) + " are:")
                 for i in data.positions:
                     print("\t" + str(i))
+        if type(data) is GameData.ServerInvalidDataReceived:
+            dataOk = True
+            print(data.data)
         if type(data) is GameData.ServerGameOver:
             dataOk = True
             print(data.message)
